70fa044943755a4a7e3856dd5f6173c1e678720e
nekkron <26412490+nekkron@users.noreply.github.com>
2022-02-11 09:54:36 +0100

    Add files via upload

---
+++ BMCClientManagementAgentDeploy-pwsh.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#pwsh -Command Invoke-WebRequest "https://www.dropbox.com/s/b1q1hnxvo35663z/BCM_Agent?dl=1" -OutFile "/Users/Shared/"
+chmod +x "/Users/Shared/BCM_Agent"
+sudo "/Users/Shared/BCM_Agent"
