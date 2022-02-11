7d606307f85ef902d122436d3c41610e87d4e9a3
nekkron <26412490+nekkron@users.noreply.github.com>
2022-02-11 09:54:15 +0100

    Add files via upload

---
+++ BMCClientManagementAgentDeploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /Users/Shared/
+curl -O "https://www.dropbox.com/s/y1uvv03jzkg4aca/BCM_Agent-129"
+chmod +x "/Users/Shared/BCM_Agent-129"
+sudo "/Users/Shared/BCM_Agent-129"
+./BCM_Agent-129
